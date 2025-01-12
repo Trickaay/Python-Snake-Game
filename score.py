0446c1b7c0e4c002dbf28046db49eca83a1b1302
Richard Cross <97173980+Trickaay@users.noreply.github.com>
2025-01-12 15:37:17 +0000

    Update score.py
    Completed the score file

--- score.py
+++ score.py
@@ -12,3 +12,14 @@ class Score(Turtle):
         
     def update_score(self):
         self.write(f"Score: {self.score}", align="center", font=("Roboto", 25, "normal"))
+        
+    def game(self):
+        self.goto(0, 0)
+        self.write("GAME OVER!!", align="center", font=("Roboto", 25, "normal"))
+    
+    def current_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
+        self.hideturtle()
+        self.goto(0, 270)
