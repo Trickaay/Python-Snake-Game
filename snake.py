0aabdce78a6436687b82889a9faffd3d8402f09c
Richard Cross <97173980+Trickaay@users.noreply.github.com>
2025-01-12 15:31:42 +0000

    Update snake.py
    Further fleshing out the snake class

--- snake.py
+++ snake.py
@@ -15,3 +15,35 @@ class Snake:
         self.snake_Len = []
         self.create_snake()
         self.head = self.snake_Len[0]
+        
+        
+    def create_snake(self):
+        for i in POS:
+            self.add_snake(i)
+    
+    def add_snake(self,i):
+        snake_create = Turtle("square")
+        snake.create.penup()
+        snake_create.color("black")
+        snake.create.setpos(i)
+        self.snake_Len.append(snake_create)
+        
+    def move(self):
+        for s in range(len(self.snake_Len) -1, 0, -1):
+            x = self.snake_Len[s - 1].xcor()
+            y = self.snake_Len[s - 1].ycor()
+            self.snake_Len[s].goto(new_x, new_y)
+        self.head.forward(MOVE_DIST)
+        
+    def levelup(self):
+        self.add_snake(self.snake_Len[-1].position())
+        
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+    
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+            
+    
