888a287bb309bf22c821d47277edc0b10d92acd2
Richard Cross <97173980+Trickaay@users.noreply.github.com>
2025-01-12 14:09:36 +0000

    First edits
    Added main.py, to define screen/surface

---
+++ snake.py
@@ -0,0 +1,17 @@
+# Simple snake game 
+#BY: Richard Cross
+
+from turtle import Turtle, Screen
+Screen = Screen()
+POS = [(0, 0) (-20, 0), (-40, 0)]
+move_distance = 20
+up = 90
+down = 270
+left = 180
+right = 0
+# creating the snake class
+class Snake:
+    def__init__(self):
+        self.snake_Len = []
+        self.create_snake()
+        self.head = self.snake_Len[0]
