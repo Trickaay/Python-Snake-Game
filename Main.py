3eb23bf6908e6aa92fbc5127704dbb330e375e58
Richard Cross <97173980+Trickaay@users.noreply.github.com>
2025-01-12 15:39:48 +0000

    Update Main.py
    Completed the main.py file

--- Main.py
+++ Main.py
@@ -5,7 +5,7 @@ from food import Food
 from score import Score
 screen = Screen()
 screen.listen()
-screen.bgcolor("black")
+screen.bgcolor("green")
 screen.title("Snake Game")
 screen.setup(width=600, height=600)
 screen.tracer(0)
@@ -22,3 +22,21 @@ game = True
 while game:
     screen.update()
     time.sleep(.1)
+    snake.move()
+    
+    if snake.head.distance(food) < 15:
+        score.current_score()
+        snake.levelup()
+        food.new_food_loc()
+        
+    for i in snake.snake_Len[1:]:
+        if snake.head.distance(i) < 10:
+            score.game()
+            break
+    
+    
+    if snake.head.xcor() > 290 or snake.head.xcor() < -290 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
+        score.game()
+        break
+
+screen.exitonclick()
